74f9598cab485dd37efdddb167e751a9844b0d6f
Matthew Alicea <matthew.ivan.alicea@gmail.com>
2022-07-24 10:38:03 -0400

    added file extension for better compatibility

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import p5 from 'p5/lib/p5';
+import p5 from 'p5/lib/p5.js';
 
 const sketch = window; // for single sketch
 window.p5 = p5;
--- sound.js
+++ sound.js
@@ -1,2 +1,2 @@
-import * as p5s from 'p5/lib/addons/p5.sound';
+import * as p5s from 'p5/lib/addons/p5.sound.js';
 // Yup, need to be on a separate file
